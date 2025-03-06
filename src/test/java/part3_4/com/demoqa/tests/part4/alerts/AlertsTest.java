99f35f837638babdd2490a77e22b99ad395d4d09
rob-moloney <rmoloney@tcd.ie>
2025-03-06 14:02:35 +0000

    Adding Confirmation Alert test case

--- src/test/java/part3_4/com/demoqa/tests/part4/alerts/AlertsTest.java
+++ src/test/java/part3_4/com/demoqa/tests/part4/alerts/AlertsTest.java
@@ -4,12 +4,11 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import part3_4.com.demoqa.base.BaseTest;
 
-import static utilities.SwitchToUtility.acceptAlert;
-import static utilities.SwitchToUtility.getAlertText;
+import static utilities.SwitchToUtility.*;
 
+@Test
 public class AlertsTest extends BaseTest {
 
-    @Test
     public void testInformationAlert() {
         String expectedAlertText = "You clicked a button";
         var alertsPage = homePage.goToAlertsWindows().clickAlerts();
@@ -17,5 +16,15 @@ public class AlertsTest extends BaseTest {
         Assert.assertEquals(getAlertText(), expectedAlertText,
                 "\n Actual & Expected Messages Do Not Match \n");
         acceptAlert();
+    }
+
+    public void testConfirmationAlert() {
+        String expectedConfirmationResult = "You selected Cancel";
+        var alertsPage = homePage.goToAlertsWindows().clickAlerts();
+        alertsPage.clickConfirmationAlertButton();
+        dismissAlert();
+        String actualConfirmationResult = alertsPage.getConfirmationResult();
+        Assert.assertEquals(actualConfirmationResult, expectedConfirmationResult,
+                "\n You Did Not Select Cancel \n");
     }
 }
